c3c4283927fe6ef97a5c405149ece13d9a0e47eb
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2024-09-19 15:30:19 -0400

    added text_node_to_html_node.py and tests

---
+++ src/test_text_node_to_html_node.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode
+from leafnode import LeafNode
+from text_node_to_html_node import text_node_to_html_node
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text_eq(self):
+        text_node = TextNode("This is a text block", "text")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "This is a text block")
+
+    def test_bold_eq(self):
+        text_node = TextNode("This is bold text", "bold")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), "<b>This is bold text</b>")
+        
+    def test_link_eq(self):
+        text_node = TextNode("Click here!", "link", "www.google.com")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), '<a href="www.google.com">Click here!</a>')
+        
+    def test_img_eq(self):
+        text_node = TextNode("Look at these moutains!", "image", "/image/mtn.jpeg")
+        html_node = text_node_to_html_node(text_node)
+        self.assertEqual(html_node.to_html(), '<img src="/image/mtn.jpeg" alt="Look at these moutains!"></img>')
+        
